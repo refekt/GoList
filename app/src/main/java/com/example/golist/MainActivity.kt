5542398d150639909b6441dd0ee5402c3cd778a8
refekt <refekt@gmail.com>
2020-11-12 20:27:10 -0500

    Initial commit

---
+++ app/src/main/java/com/example/golist/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.golist
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import kotlin.random.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Commit?
+        val test = 1
+    }
+}
